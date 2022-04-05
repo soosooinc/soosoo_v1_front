86d23feb9b89992c8f7020aa04ab69f52a86de5c
kor-kms <minsuk0704@naver.com>
2022-04-05 22:57:20 +0900

    Feat: add http get/post logic

---
+++ src/common/httpGet.ts
@@ -0,0 +1,12 @@
+import customAxios from "./axiosCustom";
+
+const httpGet = async (uri: string) => {
+  const resultData = await customAxios.get(uri);
+  if(resultData.data.status === 200) {
+    return resultData.data.data;
+  } else {
+    return null;
+  }
+};
+
+export default httpGet;

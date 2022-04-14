93f311111a447fcb471d4577d0f02d9d1c06d471
jennie <leeje2432@42maru.com>
2022-04-15 01:26:52 +0900

    Feat: add getTeachersInfo to kindergarten controller

--- src/pages/kindergarten/controller/kindergartenController.ts
+++ src/pages/kindergarten/controller/kindergartenController.ts
@@ -1,3 +1,22 @@
-const kindergartenController = () => {};
+import httpGet from "common/httpGet";
+import { useEffect, useState } from "react";
+
+const kindergartenController = () => {
+    const [teachersInfo, setTeachersInfo] = useState();
+
+    useEffect(() => {
+        getTeachersInfo();
+    }, []);
+
+    const getTeachersInfo = async (kindergartenId?: number) => {
+        kindergartenId = 1;
+        const resultData = await httpGet(`kinergarten/getTeacherInfo?kindergartenId=${kindergartenId}`);
+        setTeachersInfo(resultData);
+    };
+
+    return {
+        teachersInfo
+    };
+};
 
 export default kindergartenController;

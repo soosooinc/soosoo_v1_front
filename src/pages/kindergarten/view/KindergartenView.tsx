3169566db700f07ccc1146211262d2a9096e90a4
suzi <pc_suzi@daum.net>
2022-04-27 21:01:13 +0900

    Feat: add getKindergartenInfo to kindergarten view

--- src/pages/kindergarten/view/KindergartenView.tsx
+++ src/pages/kindergarten/view/KindergartenView.tsx
@@ -10,16 +10,26 @@ const pageTemp = (teacher: any, index: any) => {
   )
 } 
 
+const pageKindergartenInfo = (kindergartenInfo: any) => {
+  return(
+    <div>
+      <h1>유치원 : {kindergartenInfo.name}</h1>
+      <p>주소 : {kindergartenInfo.address}</p>
+      <p>대표번호 : {kindergartenInfo.phone}</p>
+    </div>
+  )
+}
+
 const KindergartenView = () => {
-  const { teachersInfo } = KindergartenController();
-  if (teachersInfo == undefined) {
+  const { teachersInfo , kindergartenInfo } = KindergartenController();
+  if (teachersInfo == undefined || kindergartenInfo==undefined) {
     return (
-      <p>유치원</p>
+      <p>유치원 정보 없음</p>
     );
   } else {
     return (
       <StyledScreenWrapper>
-        <p>유치원</p>
+        {pageKindergartenInfo(kindergartenInfo)}
         {teachersInfo.map((teacher: any, index: number) => (
           pageTemp(teacher, index)
         ))}

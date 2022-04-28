84fc839389f227f123e91acc4fae14c1446f9a4f
kor-kms <68898456+kor-kms@users.noreply.github.com>
2022-04-28 21:38:44 +0900

    Feat: add kindergarten api (#4)
    What
    - 유치원 api 추가
    
    * Feat: add getKindergartenInfo to kindergarten controller
    
    * Feat: add getKindergartenInfo to kindergarten view
    
    * Feat: typo
    
    Co-authored-by: suzi <pc_suzi@daum.net>

--- src/pages/kindergarten/view/KindergartenView.tsx
+++ src/pages/kindergarten/view/KindergartenView.tsx
@@ -7,22 +7,30 @@ const pageTemp = (teacher: any, index: any) => {
       <p>{teacher.name}</p>
       <img src={teacher.imageUrl} style={{ height: 200, width: 300 }} />
     </div>
-  )
-} 
+  );
+};
+
+const pageKindergartenInfo = (kindergartenInfo: any) => {
+  return (
+    <div>
+      <h1>유치원 : {kindergartenInfo.name}</h1>
+      <p>주소 : {kindergartenInfo.address}</p>
+      <p>대표번호 : {kindergartenInfo.phone}</p>
+    </div>
+  );
+};
 
 const KindergartenView = () => {
-  const { teachersInfo } = KindergartenController();
-  if (teachersInfo == undefined) {
-    return (
-      <p>유치원</p>
-    );
+  const { teachersInfo, kindergartenInfo } = KindergartenController();
+  if (teachersInfo === undefined || kindergartenInfo === undefined) {
+    return <p>유치원 정보 없음</p>;
   } else {
     return (
       <StyledScreenWrapper>
-        <p>유치원</p>
-        {teachersInfo.map((teacher: any, index: number) => (
+        {pageKindergartenInfo(kindergartenInfo)}
+        {teachersInfo.map((teacher: any, index: number) =>
           pageTemp(teacher, index)
-        ))}
+        )}
       </StyledScreenWrapper>
     );
   }

5e822c36d46f96775516893817f675baa2130092
jungeun <jjungeun1244@gmail.com>
2022-05-16 20:52:48 +0900

    Fix: remove unnecessary code

--- src/pages/kindergarten/controller/kindergartenController.ts
+++ src/pages/kindergarten/controller/kindergartenController.ts
@@ -1,31 +1,3 @@
-import httpGet from "common/httpGet";
-import { useEffect, useState } from "react";
+const kindergartenController = () => { };
 
-const KindergartenController = () => {
-    const [teachersInfo, setTeachersInfo] = useState<any>([]);
-    const [kindergartenInfo, setKindergartenInfo] = useState<any>([]);
-
-    useEffect(() => {
-        getTeachersInfo();
-        getKindergartenInfo();
-    }, []);
-
-    const getTeachersInfo = async (kindergartenId?: number) => {
-        kindergartenId = 1;
-        const resultData = await httpGet(`kindergarten/getTeacherInfo?kindergartenId=${kindergartenId}`);
-        setTeachersInfo(resultData);
-    };
-
-    const getKindergartenInfo = async (kindergartenId?: number) => {
-        kindergartenId = 1;
-        const resultData = await httpGet(`kindergarten/info?kindergartenId=${kindergartenId}`);
-        setKindergartenInfo(resultData);
-    };
-
-    return {
-        teachersInfo,
-        kindergartenInfo
-    };
-};
-
-export default KindergartenController;
+export default kindergartenController;
--- src/pages/kindergarten/view/KindergartenView.tsx
+++ src/pages/kindergarten/view/KindergartenView.tsx
@@ -1,45 +1,5 @@
-import { StyledScreenWrapper } from "../../../components/ScreenWrapper/styles";
-import KindergartenController from "../controller/kindergartenController";
-import Button from "../../../components/Button/index"
-
-const pageTemp = (teacher: any, index: any) => {
-  return (
-    <div key={index.toString()}>
-      <p>{teacher.name}</p>
-      <img src={teacher.imageUrl} style={{ height: 200, width: 300 }} />
-    </div>
-  );
-};
-
-const pageKindergartenInfo = (kindergartenInfo: any) => {
-  return (
-    <div>
-      <h1>유치원 : {kindergartenInfo.name}</h1>
-      <p>주소 : {kindergartenInfo.address}</p>
-      <p>대표번호 : {kindergartenInfo.phone}</p>
-    </div>
-  );
-};
-
-const buttonClickEvent = () => console.log("유치원에서 클릭했다.");
-
 const KindergartenView = () => {
-  const { teachersInfo, kindergartenInfo } = KindergartenController();
-  if (teachersInfo === undefined || kindergartenInfo === undefined) {
-    return <p>유치원 정보 없음</p>;
-  } else {
-    return (
-      <StyledScreenWrapper>
-        {pageKindergartenInfo(kindergartenInfo)}
-        <Button text="LogOut" onClick={buttonClickEvent}></Button>
-
-        {teachersInfo.map((teacher: any, index: number) =>
-          pageTemp(teacher, index)
-        )}
-        
-      </StyledScreenWrapper>
-    );
-  }
-};
+    return (<></>);
+}
 
 export default KindergartenView;

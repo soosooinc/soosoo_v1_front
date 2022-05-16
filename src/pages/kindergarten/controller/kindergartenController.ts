4213781875cb3443fc538612cc540c25557afbf0
tjddnghkrk <tjddnghkrk@gmail.com>
2022-05-16 20:53:18 +0900

    init to new architecture

--- src/pages/kindergarten/controller/kindergartenController.ts
+++ src/pages/kindergarten/controller/kindergartenController.ts
@@ -1,31 +1,7 @@
-import httpGet from "common/httpGet";
 import { useEffect, useState } from "react";
 
 const KindergartenController = () => {
-    const [teachersInfo, setTeachersInfo] = useState<any>([]);
-    const [kindergartenInfo, setKindergartenInfo] = useState<any>([]);
-
-    useEffect(() => {
-        getTeachersInfo();
-        getKindergartenInfo();
-    }, []);
-
-    const getTeachersInfo = async (kindergartenId?: number) => {
-        kindergartenId = 1;
-        const resultData = await httpGet(`kindergarten/getTeacherInfo?kindergartenId=${kindergartenId}`);
-        setTeachersInfo(resultData);
-    };
-
-    const getKindergartenInfo = async (kindergartenId?: number) => {
-        kindergartenId = 1;
-        const resultData = await httpGet(`kindergarten/info?kindergartenId=${kindergartenId}`);
-        setKindergartenInfo(resultData);
-    };
-
-    return {
-        teachersInfo,
-        kindergartenInfo
-    };
+   
 };
 
 export default KindergartenController;
--- src/pages/kindergarten/view/KindergartenView.tsx
+++ src/pages/kindergarten/view/KindergartenView.tsx
@@ -24,22 +24,7 @@ const pageKindergartenInfo = (kindergartenInfo: any) => {
 const buttonClickEvent = () => console.log("유치원에서 클릭했다.");
 
 const KindergartenView = () => {
-  const { teachersInfo, kindergartenInfo } = KindergartenController();
-  if (teachersInfo === undefined || kindergartenInfo === undefined) {
-    return <p>유치원 정보 없음</p>;
-  } else {
-    return (
-      <StyledScreenWrapper>
-        {pageKindergartenInfo(kindergartenInfo)}
-        <Button text="LogOut" onClick={buttonClickEvent}></Button>
-
-        {teachersInfo.map((teacher: any, index: number) =>
-          pageTemp(teacher, index)
-        )}
-        
-      </StyledScreenWrapper>
-    );
-  }
+  return(<></>);
 };
 
 export default KindergartenView;

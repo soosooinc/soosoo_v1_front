fd0ab2e22d5a500238d748b327bb58e3f3bdea0d
JUNG-EUN <jjungeun1244@gmail.com>
2022-06-02 20:58:33 +0900

    Feat: move Page Button in MainView (#9)
    #### What
    - 더보기 버튼으로 페이지 이동
    * Feat: add props
    
    * Feat: add more button

--- src/pages/main/controller/MainController.ts
+++ src/pages/main/controller/MainController.ts
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { getUserInfoApi } from "../../../apis/UserApis";
 import { IUserInfo } from "../../../types/User.type";
 import { Resetter, useRecoilState, useResetRecoilState } from "recoil";
@@ -7,6 +8,9 @@ import { MainNoticeHeaderTag } from "../../../constants/constants";
 
 const MainController = () => {
   const [userInfo, setUserInfo] = useRecoilState<IUserInfo>(userInfoAtom);
+  const [selectedNoticeHeaderTag, setSelectedNoticeHeaderTag] =
+    useState<number>(0);
+  const navigate = useNavigate();
   const mainNoticeHeaderTag = MainNoticeHeaderTag;
 
   useEffect(() => {
@@ -25,16 +29,32 @@ const MainController = () => {
           name: data.name,
           type: data.type,
           phone: data.phone ? data.phone : undefined,
-          email: data.email ? data.email : undefined,
+          email: data.email ? data.email : undefined
         });
       } catch (e: any) {}
     },
     [setUserInfo]
   );
 
+  const movePage = (selectedNoticeHeaderTag: number) => {
+    switch (selectedNoticeHeaderTag) {
+      case 0:
+        navigate("/notice");
+        break;
+      case 1:
+        navigate("/announce");
+        break;
+      case 2:
+        navigate("/dog");
+        break;
+    }
+  };
   return {
     userInfo,
     mainNoticeHeaderTag,
+    selectedNoticeHeaderTag,
+    setSelectedNoticeHeaderTag,
+    movePage
   };
 };
 

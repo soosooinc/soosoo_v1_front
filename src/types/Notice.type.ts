017edae5f696f0311f729d8116d10601a50c0295
kor-kms <68898456+kor-kms@users.noreply.github.com>
2022-06-30 20:52:26 +0900

    macall220614
    What
    - add notice api

--- src/types/Notice.type.ts
+++ src/types/Notice.type.ts
@@ -1,8 +1,19 @@
 export interface INoticeInfo {
-    noticeId: Number;
-    imageId: Number;
-    topId: Number;
-    type: Number;
-    mainText: string;
-    subText: string | undefined;
+  noticeId: Number;
+  imageId: Number;
+  topId: Number;
+  type: Number;
+  mainText: string;
+  subText: string | undefined;
+}
+
+export interface INotice {
+  noticeId: Number;
+  imageId: Number;
+  topId: Number;
+  kindergartenId: Number | undefined;
+  type: Number;
+  mainText: string;
+  subText: string | undefined;
+  createdAt: string;
 }

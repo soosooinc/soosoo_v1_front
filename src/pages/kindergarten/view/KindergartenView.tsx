242f665eb5e831b405c17e2f5997a8bead92921f
jungeun <jjungeun1244@gmail.com>
2022-07-07 20:54:05 +0900

    Feat: add delete button and click event

--- src/pages/kindergarten/view/KindergartenView.tsx
+++ src/pages/kindergarten/view/KindergartenView.tsx
@@ -1,8 +1,33 @@
+import { StyledScreenWrapper } from "components/ScreenWrapper/styles";
+import { convertCompilerOptionsFromJson } from "typescript";
 import kindergartenController from "../controller/kindergartenController";
 
 const KindergartenView = () => {
   const { kindergartenInfo, teacherInfo } = kindergartenController();
-  return <></>;
+  
+  function deleteTeacher(kindergartenId: number, teacherId: number) {
+    console.log(kindergartenId, teacherId);
+  }
+
+  const teachersList = teacherInfo.map(teacher => (
+    <>
+      <li key={teacher.userId}>{teacher.name}</li>
+      <li key={teacher.userId}>{teacher.userId}</li>
+      <button
+        onClick={() => {
+          deleteTeacher(kindergartenInfo.kindergartenId, teacher.userId);
+        }}
+      >
+        삭제
+      </button>
+    </>
+  ));
+
+  return (
+    <StyledScreenWrapper>
+      <ul>{teachersList}</ul>
+    </StyledScreenWrapper>
+  );
 };
 
 export default KindergartenView;

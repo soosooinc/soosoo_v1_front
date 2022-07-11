9e41f3db7b921d5862e624936a4cdb8d479cf2c2
tjddnghkrk <tjddnghkrk@gmail.com>
2022-07-11 21:50:30 +0900

    get notice list via controller

--- src/pages/notice/controller/noticeController.ts
+++ src/pages/notice/controller/noticeController.ts
@@ -1,21 +1,39 @@
-import { getNoticeInfoApi } from "apis/NoticeApis";
-import { Resetter, useRecoilState, useResetRecoilState } from "recoil";
-import { INoticeInfo } from "../../../types/Notice.type"
+import { getNoticeInfoApi, getNoticeApi } from "apis/NoticeApis";
+import { Resetter, useRecoilState, useResetRecoilState, atom } from "recoil";
+import { INoticeInfo, INotice } from "../../../types/Notice.type"
 import { noticeInfoAtom } from "../../../store/notice"
 import { useCallback, useEffect } from "react";
 
+const noticeListAtom = atom<INotice>({
+    key: "noticeListAtom",
+    default: {
+      noticeId: 0,
+      imageId: 0,
+      topId: 0,
+      kindergartenId: undefined,
+      type: 0,
+      mainText: "",
+      subText: undefined,
+      createdAt: "",
+      createdBy: ""
+    },
+  });
+
 const noticeController = () => {
     const [noticeInfo, setNoticeInfo] = useRecoilState<INoticeInfo>(noticeInfoAtom)
+    const [noticelist, setNoticeList] = useRecoilState<INotice>(noticeListAtom)
 
     useEffect(()=>{
         getNoticeInfo(1);
+        getNoticeList(1, "main", 0)
+
     }, []);
 
     const getNoticeInfo = useCallback(
         async (noticeId:number): Promise<void> =>{
         try{
             const data= await getNoticeInfoApi(noticeId);
-            console.log(data);
+            //console.log(data);
             setNoticeInfo({
                 noticeId : data.noticeId,
                 imageId: data.imageId,
@@ -31,6 +49,20 @@ const noticeController = () => {
         
         setNoticeInfo,
     ]);
+
+    const getNoticeList = useCallback(
+        async (kindergartenId:number, status:string, page:number): Promise<void> =>{
+        try{
+            const data= await getNoticeApi(kindergartenId, status, page);
+            console.log(data);
+            
+        }catch(e:any) {}
+    }, [
+        
+        setNoticeList,
+    ]);
+
+    
 
     return{
         noticeInfo,
